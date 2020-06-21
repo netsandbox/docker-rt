ae62cda892ee7fefff56d7c7b4a558b66409421c
Christian Loos <cloos@netsandbox.de>
2020-06-21 17:06:06 +0200

    update update.sh to support alpha and beta releases and RT 5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 : "${RT_WEB_PORT:=80}"
 
-sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt%%RT_VERSION_MAJOR%%/etc/RT_SiteConfig.pm
 
 exec "$@"
--- update.sh
+++ update.sh
@@ -6,17 +6,24 @@ declare -A versions=(
   [4.0.25]='69daa9b9e6c9acb4ca31ec1c3efc8bb4901cc7047eed784f2f91515815fdd4cd'
   [4.2.16]='1bbe619072b05efb55725c9df851363892b77ad6788dfd28eadce6a8f84a8209'
   [4.4.4]='34c316a4a78d7ee9b95d4391530f9bb3ff3edd99ebbebfac6354ed173e940884'
+  [5.0.0beta2]='58687fd1103c04a4f0b622e54d1d4097f36d45aa4d744374add2f76591ceb0ff'
 )
 travis_env=
 for rt_version in "${!versions[@]}"; do
-  rt_sha=${versions[$rt_version]}
   dir=${rt_version:0:3}
+  rt_sha=${versions[$rt_version]}
   travis_env+="\n  - VERSION=$dir"
 
   if [ "$dir" = '4.0' ]; then
     rt_configure='--disable-gpg --enable-gd --enable-graphviz --with-db-type=SQLite --with-devel-mode \\'
   else
     rt_configure='--disable-gpg --disable-smime --enable-developer --enable-gd --enable-graphviz --with-db-type=SQLite \\'
+  fi
+
+  if [[ "$rt_version" == *"alpha"* ]] || [[ "$rt_version" == *"beta"* ]]; then
+    rt_release='devel'
+  else
+    rt_release='release'
   fi
 
   mkdir -p $dir
@@ -30,10 +37,12 @@ for rt_version in "${!versions[@]}"; do
   cp -a Dockerfile.template $dir/Dockerfile
 
   sed -i \
-    -e "s/%%RT_VERSION%%/$rt_version/" \
-    -e "s/%%RT_SHA%%/$rt_sha/" \
     -e "s/%%RT_CONFIGURE%%/$rt_configure/" \
-    $dir/Dockerfile
+    -e "s/%%RT_RELEASE%%/$rt_release/" \
+    -e "s/%%RT_SHA%%/$rt_sha/" \
+    -e "s/%%RT_VERSION_MAJOR%%/${rt_version:0:1}/" \
+    -e "s/%%RT_VERSION%%/$rt_version/" \
+    $dir/apache.rt.conf $dir/docker-entrypoint.sh $dir/Dockerfile
 done
 
 travis_env=$(echo -e "$travis_env" | sort | sed ':a;N;$!ba;s/\n/\\n/g')
