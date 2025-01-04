f871075569f1a872ac46feb95aa3cefa524503ac
Christian Loos <cloos@netsandbox.de>
2025-01-04 18:16:43 +0100

    update download check

--- update.sh
+++ update.sh
@@ -2,12 +2,9 @@
 
 set -Eeuo pipefail
 
-declare -A versions=(
-  [4.4.7]='47af1651d5df3f25b6374ff6c1da71c66202d61919d9431c17259fa3df69ae59'
-  [5.0.7]='1a1a4838979f08c58e67642686e4fd980e1f4ee98e144ff8a56f870f37162cce'
-)
+declare -a versions=("4.4.7" "5.0.7")
 
-for version in "${!versions[@]}"; do
+for version in "${versions[@]}"; do
   version_major_minor=${version%.*}
 
   mkdir -p "$version_major_minor"
@@ -23,7 +20,6 @@ for version in "${!versions[@]}"; do
 
   sed -i \
     -e "s/%%RT_RELEASE%%/$release/" \
-    -e "s/%%RT_SHA%%/${versions[$version]}/" \
     -e "s/%%RT_VERSION_MAJOR%%/${version%%.*}/" \
     -e "s/%%RT_VERSION%%/$version/" \
     "$version_major_minor"/docker-entrypoint.sh "$version_major_minor"/Dockerfile
